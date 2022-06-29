2deb00ed80e0b5b2d77580cdfe3c6728c1626a8e
Macarena <Macarena@DESKTOP-5A2HH1S>
2022-06-29 11:47:05 +0200

    Con ejercicio extra

---
+++ Bucles/src/EjercicioExtra.java
@@ -0,0 +1,26 @@
+/** Pedir 10 numeros por teclado y calcular el menor.
+ */
+
+import java.util.Scanner;
+
+public class EjercicioExtra {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.printf("Introduce el 0 numero\n");
+		int num = entrada.nextInt();
+		int menor=num;
+		
+		for(int i=1;i<10;i++)	
+		{
+			System.out.printf("Introduce el %d numero\n",i);
+			num = entrada.nextInt();
+			
+			if (num<menor)
+				menor=num;
+		}
+		System.out.printf("El menor es: %d",menor);
+		
+	}
+
+}
